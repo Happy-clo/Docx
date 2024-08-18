c6413397dafa87e4ccb99f3c90c0bcffc8db2e7b
Happy-clo <support@wdsj.one>
2024-08-18 19:37:14 +0800

    docs(config): 更新版权年份和持有人
    将版权年份更新为当前年份，并将版权持有人从"Artaphy & Happy-clo Docs, Inc."更改为"Artaphy Docs, Inc."，以反映公司名称的更改。

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Artaphy & Happy-clo Docs, Inc.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Artaphy Docs, Inc.`,
       },
       prism: {
         theme: prismThemes.github,
